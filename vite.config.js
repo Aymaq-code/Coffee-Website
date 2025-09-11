0dcd263aa03908c59b787d2691f0a04cd14e1bab
Aymaq <rashidaymaq1994@gmail.com>
2025-09-11 18:19:53 +0430

    under proccess

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  preview: {
+    port: 10000,
+    host: "0.0.0.0",
+    allowedHosts: ["renderr-sv80.onrender.com"],
+  },
 });
